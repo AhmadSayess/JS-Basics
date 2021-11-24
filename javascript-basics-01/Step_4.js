ac592a8aad3f9963064cd6d8460d775569766b95
AhmadSayess <ahmadsayess1998@gmail.com>
2021-11-24 14:37:58 +0200

    basics-01-step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,14 @@
+ let name= window.prompt("enter your name please");
+
+ let surname = window.prompt("enter your surname please");
+
+ let city = window.prompt("enter your city please")
+
+ 
+
+
+ alert(`Nom: ${name}
+        prenom:${surname}
+        Ville :${city}
+         `
+     );
